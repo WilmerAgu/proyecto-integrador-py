e7f17ec99382fd4907a80c65d58d260409a78dea
AlejaGQ <aygonzalezqu@cesde.net>
2024-10-17 09:45:15 -0500

    Se añade información de integrantes del equipo

--- Inicio.py
+++ Inicio.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide", page_title="Mapping Demo", page_icon="🌍")
 
 
 # Título y subtítulo
-st.title("Proyecto Integrador: [Nombre del Proyecto]")
+st.title("Proyecto Integrador: Factura Pro")
 st.subheader("Un Viaje Creativo con [Nombre del Equipo]")
 
 # Imagen de fondo
@@ -15,7 +15,7 @@ st.image(image, width=700, use_column_width=True)
 # Integrantes
 st.header("Nuestro Equipo")
 
-col1, col2 = st.columns(2)
+col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
     st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
@@ -26,6 +26,23 @@ with col2:
     st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
     st.write("**[Nombre del Integrante 2]**")
     st.write("[Rol en el proyecto]")
+
+with col3:
+    st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
+    st.write("**[Nombre del Integrante 3]**")
+    st.write("[Rol en el proyecto]")
+
+with col4:
+    st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
+    st.write("**[Nombre del Integrante 4]**")
+    st.write("[Rol en el proyecto]")
+
+with col5:
+    st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
+    st.write("**[Nombre del Integrante 5]**")
+    st.write("[Rol en el proyecto]")
+    
+    
 
 # Descripción del proyecto
 st.header("Sobre el Proyecto")
